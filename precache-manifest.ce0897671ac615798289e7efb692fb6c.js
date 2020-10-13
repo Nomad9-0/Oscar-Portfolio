dd12f16944bbdb0471ee775502b4d4a73d9d8b71
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-10-12 17:08:41 -0700

    Updates

---
+++ precache-manifest.ce0897671ac615798289e7efb692fb6c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99368cad8e32f3a25b584e4ddddb729c",
+    "url": "/Oscar-Portfolio/index.html"
+  },
+  {
+    "revision": "e16feca4c56437783840",
+    "url": "/Oscar-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a59d83181f29bc82f788",
+    "url": "/Oscar-Portfolio/static/css/main.5167caee.chunk.css"
+  },
+  {
+    "revision": "e16feca4c56437783840",
+    "url": "/Oscar-Portfolio/static/js/2.2494ac24.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Oscar-Portfolio/static/js/2.2494ac24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59d83181f29bc82f788",
+    "url": "/Oscar-Portfolio/static/js/main.fe579dca.chunk.js"
+  },
+  {
+    "revision": "4f04c97e1a5668454fa4",
+    "url": "/Oscar-Portfolio/static/js/runtime-main.b3b3b38d.js"
+  },
+  {
+    "revision": "d0287f6d4f8c34bcd3db4bb1aa2f757f",
+    "url": "/Oscar-Portfolio/static/media/currency.d0287f6d.JPG"
+  },
+  {
+    "revision": "14cefb171dd64fd5ae8c948912874de3",
+    "url": "/Oscar-Portfolio/static/media/doggy.14cefb17.JPG"
+  },
+  {
+    "revision": "e346071b134c8bafd6fd915cf9e2d8b7",
+    "url": "/Oscar-Portfolio/static/media/friend-finder.e346071b.JPG"
+  },
+  {
+    "revision": "e9721b0d16308be6adcc373f9a05b808",
+    "url": "/Oscar-Portfolio/static/media/mentor-me.e9721b0d.jpg"
+  },
+  {
+    "revision": "08ab65fe649350d0018ba497ddf7777e",
+    "url": "/Oscar-Portfolio/static/media/nomad.08ab65fe.JPG"
+  },
+  {
+    "revision": "684d026dbd9fd91bf74a88bee3b15116",
+    "url": "/Oscar-Portfolio/static/media/oscar.684d026d.jpg"
+  },
+  {
+    "revision": "32d4369c2f939d7602592575c9611fe3",
+    "url": "/Oscar-Portfolio/static/media/roots.32d4369c.JPG"
+  }
+]);
